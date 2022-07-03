f27e179b965092163857c88029d5725af98f5bb2
Ebrahim <ebrahimmansur90117@gmail.com>
2022-07-03 21:51:50 +0200

    ✅  add test
      - add decorateMessag test

--- emansur_logger/test/emansur_logger_test.dart
+++ emansur_logger/test/emansur_logger_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:emansur_logger/emansur_logger.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('should return message [🎉{message}🎉]', () {
+    const input = 'hello';
+    const matcher = '🎉hello🎉';
+    final message = printMessage(message: input);
+    expect(message, matcher);
   });
 }
