e2638c64e58518f8674ebbe7a662fab0829ffd11
Ebrahim <ebrahimmansur90117@gmail.com>
2022-07-03 23:55:49 +0200

    ⚰️  remove test code

--- emansur_logger/test/emansur_logger_test.dart
+++ emansur_logger/test/emansur_logger_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:emansur_logger/emansur_logger.dart';
-
 void main() {
   test('should return message [🎉{message}🎉]', () {
-    const input = 'hello';
-    const matcher = '🎉hello🎉';
-    final message = decorateMessage(message: input);
-    expect(message, matcher);
+    // const input = 'hello';
+    // const matcher = '🎉hello🎉';
+    // final message = decorateMessage(message: input);
+    // expect(message, matcher);
   });
 }
